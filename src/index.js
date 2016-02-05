7e1744f112ada699c03022f5f8caed70ec33d759
Urban Faubion <urban.faubion@gmail.com>
2016-02-05 16:13:46 +0100

    Refactored module into a single file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /* @flow */
 import invariant from 'invariant'
-import baseConfig from './base-config'
-import { DefinePlugin, HotModuleReplacementPlugin } from 'webpack'
+import { DefinePlugin, NoErrorsPlugin, HotModuleReplacementPlugin } from 'webpack'
 import ExtractTextPlugin, { extract } from 'extract-text-webpack-plugin'
+import autoprefixer from 'autoprefixer'
 import { smart as merge } from 'webpack-merge'
 
 const ENV = process.env.NODE_ENV || 'development'
@@ -11,34 +11,49 @@ const DEV = !PROD
 const BUILD_DEV = process.env.BUILD_DEV || DEV
 const BUILD_PRERELEASE = process.env.BUILD_PRERELEASE || false
 
-function getConfig (config: Object, isHot: boolean = DEV): Object {
+const baseConfig: Object = {
+  output: {
+    filename: '[name].js',
+    cssFilename: '[name].css',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json'],
+    packageAlias: 'browser'
+  },
+  stats: { colors: true, timings: true },
+  plugins: [ new NoErrorsPlugin() ],
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['babel'], exclude: /node_modules/ },
+      { test: /\.json$/, loaders: ['json'] },
+      { test: /\.jpe?g$|\.gif$|\.png$/i, loader: 'url?limit=10000' },
+      { test: /\.svg|\.otf|\.eot|\.woff|\.woff2|\.ttf/i,
+        loader: 'url?limit=100000' }
+    ]
+  },
+
+  postcss: () => [autoprefixer]
+}
+
+export default function (config: Object, isDev: boolean = DEV): Object {
   invariant(
     config && config.context && config.entry && config.output && config.output.path,
     'Must pass in config options object with `context`,  `entry` and `output.path` properties.'
   )
 
-  const cssLocalIdentityName = '[name]__[local]___[hash:base64:5]'
-  const cssModules = `css?modules&importLoaders=1&localIdentityName=${cssLocalIdentityName}`
-  const stylesheetsLoaders = ['style', cssModules, 'postcss']
   const cssFilename = config.output.cssFilename || baseConfig.output.cssFilename
+  const cssLoaders = isDev
+    ? 'style!css?modules!postcss'
+    : extract('style', 'css?modules!postcss')
 
   return merge(baseConfig, config, {
-    cache: DEV,
-    ...(isHot && {
-      devtools: 'cheap-module-source-map'
-    }),
-    stats: {
-      reasons: DEV
-    },
+    cache: isDev,
+    devtools: isDev ? 'cheap-module-eval-source-map' : 'source-map',
+    stats: { reasons: isDev },
     module: {
       loaders: [
-        {
-          test: /\.css$/,
-          loader: (isHot
-            ? stylesheetsLoaders.join('!')
-            : extract('style', stylesheetsLoaders.slice(1).join('!'))
-          )
-        }
+        { test: /\.css$/, loader: cssLoaders }
       ]
     },
     plugins: [
@@ -46,12 +61,10 @@ function getConfig (config: Object, isHot: boolean = DEV): Object {
         __DEV__: BUILD_DEV,
         __PRERELEASE__: BUILD_PRERELEASE
       }),
-      ...(isHot
+      ...(isDev
         ? [new HotModuleReplacementPlugin()]
         : [new ExtractTextPlugin(cssFilename, { allChunks: true })]
       )
     ]
   })
 }
-
-export default getConfig
